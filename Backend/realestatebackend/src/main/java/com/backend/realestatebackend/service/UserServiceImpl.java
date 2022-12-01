e5f6f97800b8a0dab512dc2dc99496fed8a9506e
Mathew Prince <mathewprince.dev08@gmail.com>
2022-12-02 00:23:51 +0530

    UserSeriviceImpl update

--- Backend/realestatebackend/src/main/java/com/backend/realestatebackend/service/UserServiceImpl.java
+++ Backend/realestatebackend/src/main/java/com/backend/realestatebackend/service/UserServiceImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 
 import com.backend.realestatebackend.collections.TokenCollection;
 import com.backend.realestatebackend.collections.UserCollection;
-import com.backend.realestatebackend.fields.VerificationToken;
+import com.backend.realestatebackend.fields.TokenFields.VerificationToken;
 import com.backend.realestatebackend.models.UserModel;
 
 @Service
@@ -37,15 +37,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserCollection registerNewUser(UserModel userModel) {
         UserCollection user = new UserCollection();
-        
+
         user.setName(userModel.getName());
         user.setEmail(userModel.getEmail());
         user.setPassword(userModel.getPassword());
         user.setPhoneNumber(userModel.getPhoneNumber());
-        
 
         user.setDp(cloudinaryService.uploadToCloudinary(assetProvider.getDpFile(), userModel.getName(), "default_dp"));
-        
+
         Date currentDate = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
         String joiningDate = formatter.format(currentDate);
@@ -85,20 +84,22 @@ public class UserServiceImpl implements UserService {
     public String validateVerficationToken(String token) {
         Query query = new Query().addCriteria(Criteria.where("verificationToken.token").is(token));
         TokenCollection tokenObj = mt.findOne(query, TokenCollection.class);
-        if(tokenObj == null) return "Invalid";
+        if (tokenObj == null)
+            return "Invalid";
 
         Calendar cal = Calendar.getInstance();
-        if(tokenObj.getVerificationToken().getExpirationTime().getTime() - cal.getTime().getTime() < 0){
+        if (tokenObj.getVerificationToken().getExpirationTime().getTime() - cal.getTime().getTime() < 0) {
             mt.remove(tokenObj);
             return "Expired";
         }
 
         UserCollection user = mt.findById(tokenObj.getUserId(), UserCollection.class);
-        if(user == null) return "Cannot find the user";
+        if (user == null)
+            return "Cannot find the user";
         user.setEnabled(true);
         mt.save(user);
         logger.info(user.toString());
         return "Valid";
     }
-    
+
 }
