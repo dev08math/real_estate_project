ceda6625eca2d335ee757a627698680a7e714ba2
Mathew Prince <mathewprince.dev08@gmail.com>
2022-12-02 00:22:30 +0530

    Updatd User collection

--- Backend/realestatebackend/src/main/java/com/backend/realestatebackend/collections/UserCollection.java
+++ Backend/realestatebackend/src/main/java/com/backend/realestatebackend/collections/UserCollection.java
@@ -25,6 +25,9 @@ public class UserCollection {
     @Builder.Default
     private  boolean enabled = false;
 
+    @Builder.Default
+    private  boolean visibility = false;
+
     // @Transient
     // private MultipartFile dp;
 
